aab78f126b9bbf2de92549c412ff56d8a1c03490
Spoonflower <mhowell@spoonflower.com>
2016-06-22 18:54:44 -0400

    trying to get login to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,29 @@
-Rails.application.routes.draw do
-  get 'users/new'
+# Rails.application.routes.draw do
+#   get 'users/new'
+#
+#   root                 'static_pages#home'
+#   get 'board' =>      'static_pages#board'
+#   get 'messages' =>   'static_pages#messages'
+#   get    'signup'  => 'users#new'
+#   get    'login'   => 'sessions#new'
+#   post   'login'   => 'sessions#create'
+#   resources :users
+# end
 
-  root                 'static_pages#home'
-  get 'board' =>    'static_pages#board'
-  get 'messages' => 'static_pages#messages'
+
+Rails.application.routes.draw do
+  root                'static_pages#home'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
